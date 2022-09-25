61a8d11eca722fa4a4fd713fc835af9840305ac2
tygr <graytd1996@gmail.com>
2022-09-25 16:59:57 -0400

    Add js for outline, eraser and new game functions

--- java.js
+++ java.js
@@ -1,7 +1,13 @@
 const playWindow = document.querySelector('.play-window');
-const button = document.querySelector('button');
+const button1 = document.querySelector('.btn1');
+const button2 = document.querySelector('.btn2');
+const button3 = document.querySelector('.btn3');
+const button4 = document.querySelector('.btn4');
 
-button.addEventListener('click', createSquares);
+button1.addEventListener('click', createSquares);
+button2.addEventListener('click', toggleOutline);
+button3.addEventListener('click',eraser);
+button4.addEventListener('click',draw);
 
 function buttonNumber() {
     number = prompt("Enter a number less than or equal to 100!");
@@ -9,6 +15,32 @@ function buttonNumber() {
         number = prompt("Enter a number less than or equal to 100!");
     }
     return number;
+}
+function draw(){
+    const color = document.querySelectorAll('.play-window>div');
+    color.forEach((colors) => {
+        colors.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'gray';
+        });
+    }); 
+}
+function eraser(){
+    const remove = document.querySelectorAll('.play-window>div');
+    const playWindow= document.querySelector('.play-window');
+    remove.forEach((removes)=> {
+        removes.addEventListener('mouseover',(e)=>{
+            e.target.style.background = playWindow.style.background;
+        });
+    });
+    
+}
+
+function toggleOutline(){
+    const color = document.querySelectorAll('.play-window>div');
+    color.forEach((colors)=>{
+        colors.classList.toggle('squares');
+    });
+ 
 }
 
 //code to create squares for drawing 
@@ -40,13 +72,10 @@ function createSquares() {
     };
     
     //Add mouseover even listener to all div's in playWindow
-    const color = document.querySelectorAll('.squares');
-    color.forEach((colors) => {
-        colors.addEventListener('mouseover', (e) => {
-            e.target.style.background = 'gray';
-        });
-    }); 
+    draw();
 }
+
+
 
 
 
