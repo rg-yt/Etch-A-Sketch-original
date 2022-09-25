0f0a083a619e23969f92e609e106fbd3bd11e82c
tygr <graytd1996@gmail.com>
2022-09-24 22:33:06 -0400

    Fix button function to remove any squares first

--- java.js
+++ java.js
@@ -1,20 +1,38 @@
 const playWindow = document.querySelector('.play-window');
-const squares = document.createElement('div');
+const button = document.querySelector('button');
 
-    
-//code to create squares for drawing 
-for(let x = 0; x < 256; x++){
-    const squares = document.createElement('div');  
-    playWindow.appendChild(squares);
-    squares.classList.add('squares');
+button.addEventListener('click', createSquares);
+
+function buttonNumber() {
+    number = prompt('Enter a number!');
+    return number;
 }
 
-//code to recolor squares 
-const color = document.querySelectorAll('.squares');
-
-color.forEach((colors) => {
-    colors.addEventListener('mouseover' , (e) => {
-        e.target.style.background = 'gray';
-    });
+//code to create squares for drawing 
+function createSquares() {
     
-  });
+    buttonNumber();
+    
+    //Removes the squares while there are any squares in the play window. 
+    while(playWindow.firstChild){
+        playWindow.removeChild(playWindow.firstChild);
+    }
+    
+    for (let x = 0; x < number * number; x++) {
+        const squares = document.createElement('div');
+        playWindow.appendChild(squares);
+        squares.style.cssText = `width:${800 / number}px; height: ${800 / number}px`;
+        squares.classList.add('squares');
+    };
+    
+    //Add mouseover even listener to all div's in playWindow
+    const color = document.querySelectorAll('.squares');
+    color.forEach((colors) => {
+        colors.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'gray';
+        });
+    }); 
+}
+
+
+
